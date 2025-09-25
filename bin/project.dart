22f4b45dbfc8e482efe577869957359ef56e1117
alfbrnsy <aldofebriansyah0224@gmail.com>
2025-09-25 19:27:08 +0700

    membuat daftar menghitung menggunakan union dan intersection

--- bin/project.dart
+++ bin/project.dart
@@ -3,21 +3,37 @@ import 'package:project/project.dart' as project;
 void main(List<String> arguments) {
   print('Hello world: ${project.calculate()}!');
 
-  // Buat growable list untuk menyimpan nama mahasiswa
-  List<String> daftarMahasiswa = <String>[];
+  // // Buat growable list untuk menyimpan nama mahasiswa
+  // List<String> daftarMahasiswa = <String>[];
 
-  // Tambahkan nama-nama mahasiswa ke list (growable, jadi bisa ditambah)
-  daftarMahasiswa.add('Aldoi');
-  daftarMahasiswa.add('Budi');
-  daftarMahasiswa.add('Citra');
-  daftarMahasiswa.add('Dewi');
-  daftarMahasiswa.add('Eko');
+  // // Tambahkan nama-nama mahasiswa ke list (growable, jadi bisa ditambah)
+  // daftarMahasiswa.add('Aldoi');
+  // daftarMahasiswa.add('Budi');
+  // daftarMahasiswa.add('Citra');
+  // daftarMahasiswa.add('Dewi');
+  // daftarMahasiswa.add('Eko');
 
-  // Tampilkan seluruh daftar nama
-  print('Daftar Nama Mahasiswa:');
-  for (int i = 0; i < daftarMahasiswa.length; i++) {
-    print('${i + 1}. ${daftarMahasiswa[i]}');
-  }
+  // // Tampilkan seluruh daftar nama
+  // print('Daftar Nama Mahasiswa:');
+  // for (int i = 0; i < daftarMahasiswa.length; i++) {
+  //   print('${i + 1}. ${daftarMahasiswa[i]}');
+  // }
 
-  // Tampilkan jumlah data
+  // // Tampilkan jumlah data
+
+//membuat daftar union dan intersection
+ 
+  Set<String> setA = {'A', 'B', 'C'};
+  Set<String> setB = {'B', 'C', 'D'};
+
+  Set<String> union = setA.union(setB);
+  Set<String> intersection = setA.intersection(setB);
+
+  print('Union: $union');
+  print('Intersection: $intersection');
+
+
+
 }
+
+
